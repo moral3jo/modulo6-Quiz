b815f342bc67a8851eb269d4f60ee958ac1890a4
moral3jo <moral3jo@gmail.com>
2015-07-12 20:24:15 +0200

    Autoload

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load);  // autoload :quizId
+
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
